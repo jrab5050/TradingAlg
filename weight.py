0f4e92b5a10676fee80c785dba7a56f6fedf832d
Satoshi <CryBabyTwo9@protonmail.com>
2021-08-11 21:16:35 -0400

    Still working on faster alg for assigning weights, will probaly try recursive approach tommorow

--- weight.py
+++ weight.py
@@ -4,14 +4,80 @@ from random import randint, random
 from random import seed
 from tabulate import tabulate
 
+seed(randint(1, 100))
+size = 10
+rndList = [randint(1,100) * random() for x in range(size)]
+adict = {1:[], 2:[0 for x in range(size)]}
+num = randint(1,100)
+ignoreIndx = []
+
 def diff(x,y):
     if x >= y:
         return abs((x - y) / x)
 
     return abs((y - x) / y)
 
-def main():
-    size = 100
+def weightAlg2(size: int):
+    # 1: The rand nums, 2: weights for rand nums
+    global adict
+    global rndList
+    global num
+    global ignoreIndx
+
+    weights = [ x / size for x in range(size) ]
+
+    adict[1] = rndList
+    i = 0
+    ourNum = num
+    # assume the first element is the closest
+    closest = rndList[0]
+    checks = [False for x in range(size)]
+    done = False # we will be done when all checks are true
+    currWeight = weights.pop()
+    adict[2][0] = currWeight
+
+    print(f'num is {num}: Alg2')
+
+
+    while not done and len(weights) > 0:
+        if diff(num,rndList[i]) < diff(ourNum,closest) and i not in ignoreIndx:
+            # print('in if 1')
+            adict[2][i] = currWeight
+            # print(f'Adding weight to index {i}')
+            closest = rndList[i]
+
+        if i == size - 1:
+            # print('in if 2')
+            ignoreIndx.insert(0,adict[1].index(closest))
+            # print(f'The index of the closest value {adict[1].index(closest)}')
+            # print(tabulate({'num': [num], 'closest': [closest]}, headers='keys',tablefmt='fancy_grid'))
+            checks[adict[1].index(closest)] = True
+            currWeight = weights.pop()
+            ourNum = closest 
+            if adict[1].index(closest) not in ignoreIndx:
+                closest = adict[1][adict[1].index(closest)]
+            else:
+                closest = adict[1][0]
+            i = 0
+
+        else:
+            # print('in else')
+            i += 1
+
+        if False not in checks:
+            # print('In if 3')
+            done = True
+
+        # print(f'i is {i}')
+
+def weightAlg1(size: int):
+
+    global adict
+    global rndList
+    global num
+
+    weights = [ x / size for x in range(size) ]
+
     seed(randint(1, 100))
     weights = [ x / size for x in range(size) ]
     rndList = [randint(1,100) * random() for x in range(size)]
@@ -24,7 +90,7 @@ def main():
     adict[1] = rndList
 
     # pprint.pp(adict[1])
-    # print(f'num is {num}')
+    print(f'num is {num}: Alg1')
 
     i = 0
     lst = []
@@ -43,10 +109,14 @@ def main():
         adict[2][lst.index(min(lst))] = weights.pop()
         lst[lst.index(min(lst))] = 999.999
 
+
+def main():
+    # weightAlg1(size)
     # print(tabulate(adict, headers='keys', tablefmt='fancy_grid'))
+    weightAlg2(size)
+    print(tabulate(adict, headers='keys', tablefmt='fancy_grid'))
 
 if __name__ == '__main__':
     startTime = time.time()
     main()
     elapsedTime = time.time() - startTime
-    print(f'It took {elapsedTime}s')
